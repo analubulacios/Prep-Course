709dd45d000037fb81758d880846d15d77c3939a
Ana Lucia Bulacios Arroyo <analuciabulaciosarroyo@Anas-MacBook-Pro.local>
2022-05-12 12:53:58 -0300

    agregando02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -215,7 +215,7 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra==='a'||letra==='e'||letra==='i'||letra==='o'||letra==='u') {return 'Es vocal';}
-  else if (letra.legth>1) {return 'Dato incorrecto';}
+  else if (letra.length>1) {return 'Dato incorrecto';}
   else {return 'Dato incorrecto';}
 }
 
